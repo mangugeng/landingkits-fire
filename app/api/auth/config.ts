2b45fd31d151cdf8e04c37265523dae6e4a640df
mangugeng <mangugeng@yahoo.com>
2025-03-21 07:14:50 +0700

    fix: update environment variables configuration

--- app/api/auth/config.ts
+++ app/api/auth/config.ts
@@ -11,7 +11,7 @@ export const authConfig = {
   ],
   adapter: FirestoreAdapter({
     credential: cert({
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
